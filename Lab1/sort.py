7eba4905949cfeead66cae965bc8ae7d14026b6e
blueetfm <tan.yx2004@gmail.com>
2025-02-09 17:06:11 +0800

    basic structure of part a)

--- Lab1/sort.py
+++ Lab1/sort.py
@@ -1,18 +1,30 @@
 '''
-In Mergesort, when the sizes of subarrays are small, the overhead of many recursive
-calls makes the algorithm inefficient. Therefore, in real use, we often combine
-Mergesort with Insertion Sort to come up with a hybrid sorting algorithm for better
-efficiency. The idea is to set a small integer S as a threshold for the size of subarrays.
+ The idea is to set a small integer S as a threshold for the size of subarrays.
 Once the size of a subarray in a recursive call of Mergesort is less than or equal to S,
 the algorithm will switch to Insertion Sort, which is efficient for small-sized input.
+
 (a) Algorithm implementation: Implement the above hybrid algorithm.
 '''
 
-def insertion_sort(arr):
-    return
+def insertion_sort(arr) -> []:
 
 
-def mergesort(low, high, arr, s):
+    return arr
+
+def merge
+
+def mergesort(low, high, arr, s) -> None:
+    if (high - low + 1) <= s:
+        insertion_sort(arr)
+
     mid = (low + high)/2
+    if high <= low:
+        return
+    elif (high - low) > 1:
+        mergesort(low, mid, arr, s)
+        mergesort(mid + 1, high, arr, s)
+    
+    merge(low, high)
+
 
     return
