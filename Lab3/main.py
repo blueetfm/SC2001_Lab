e2bf822605e88c09c1fe8323d821d16da8b49786
selen-nium <153980794+selen-nium@users.noreply.github.com>
2025-04-05 14:08:45 +0800

    update agn

--- Lab3/main.py
+++ Lab3/main.py
@@ -1,5 +1,12 @@
 from knapsack import knapsack, print_matrix, unbounded_knapsack
 
+def part_4a(weights, profits, capacity, n):
+    print(f"\n--- Running knapsack for capacity = {capacity}, weights = {weights}, profits = {profits} ---")
+    res_matrix, max_profit = knapsack(weights, profits, capacity, n)
+    print_matrix(weights, profits, res_matrix)
+
+    print(f"The result of P(14) with weights [4, 6, 8] and profits [7, 6, 9] is {max_profit}.")
+
 # Example 1:
 weights1 = [4, 6, 8]
 profits1 = [7, 6, 9]
